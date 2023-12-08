7b3aefa7b5249101459020c44c4cd61190efe009
pinomaker-hoo <inhoo23@naver.com>
2023-12-08 23:37:01 +0900

    init : Create Project

---
+++ src/main/java/payhere/cafeproduct/CafeProductApplication.java
@@ -0,0 +1,13 @@
+package payhere.cafeproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/payhere/cafeproduct/CafeProductApplicationTests.java
@@ -0,0 +1,13 @@
+package payhere.cafeproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
