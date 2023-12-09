192920dd573055b939e0ed3c1fc645830dc15375
pinomaker-hoo <inhoo23@naver.com>
2023-12-09 16:20:36 +0900

    feat : Add Global Exception Handler

---
+++ src/main/java/payhere/cafeproduct/global/handler/GlobalExceptionHandler.java
@@ -0,0 +1,71 @@
+package payhere.cafeproduct.global.handler;
+
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import payhere.cafeproduct.global.dto.ApiResponse;
+import payhere.cafeproduct.global.dto.MetaResponse;
+import payhere.cafeproduct.global.exception.*;
+
+import java.rmi.ServerException;
+
+import static org.springframework.http.HttpStatus.*;
+
+/**
+ * 전역 에러 처리 핸들러
+ */
+@Slf4j
+@NoArgsConstructor
+@RestControllerAdvice(basePackages = {"boilerplate.pinomaker"})
+public class GlobalExceptionHandler {
+    @ExceptionHandler({
+            UnAuthenticationException.class
+    })
+    public ResponseEntity<Object> handleUnauthorizedException(Exception e) {
+        return errorResponse(e, UNAUTHORIZED);
+    }
+
+    @ExceptionHandler({
+            BadRequestException.class
+    })
+    public ResponseEntity<Object> handleBadRequestException(Exception e) {
+        return errorResponse(e, BAD_REQUEST);
+    }
+
+    @ExceptionHandler({
+            MethodArgumentNotValidException.class,
+            ValidationException.class,
+    })
+    public ResponseEntity<Object> handleValidationException(Exception e) {
+        return ApiResponse.builder().meta(new MetaResponse(BAD_REQUEST, e.getMessage())).data(null).buildObject();
+    }
+
+    @ExceptionHandler({ForbiddenException.class})
+    public ResponseEntity<Object> handleForbiddenException(Exception e) {
+        return errorResponse(e, FORBIDDEN);
+    }
+
+    @ExceptionHandler({NotFoundException.class})
+    public ResponseEntity<Object> handleNotFoundException(Exception e) {
+        return errorResponse(e, NOT_FOUND);
+    }
+
+    @ExceptionHandler({Exception.class, RuntimeException.class})
+    public ResponseEntity<Object> handleInternalServerError(Exception e) {
+        return errorResponse(e, INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ServerException.class)
+    protected ResponseEntity<Object> handleServerException(final ServerException e) {
+        return errorResponse(e, INTERNAL_SERVER_ERROR);
+    }
+
+    private ResponseEntity<Object> errorResponse(Exception e, HttpStatus httpStatus) {
+        log.error("[LOG] {} ({}) : {}", httpStatus, httpStatus.value(), e.getMessage());
+        return ApiResponse.builder().meta(new MetaResponse(httpStatus, e.getMessage())).data(null).buildObject();
+    }
+}
