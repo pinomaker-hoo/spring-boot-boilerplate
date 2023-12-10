d743266f751e77329c2b84cad59feefee1f54b86
pinomaker-hoo <inhoo23@naver.com>
2023-12-11 01:29:48 +0900

    feat : Add Base Time Entity

---
+++ src/main/java/payhere/cafeproduct/global/domain/BaseTimeCreatedEntity.java
@@ -0,0 +1,19 @@
+package payhere.cafeproduct.global.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.hibernate.annotations.Comment;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeCreatedEntity {
+    @Comment("생성 시간")
+    @Column(nullable = false, name = "created_date")
+    @CreatedDate
+    private LocalDateTime createdDate;
+}
