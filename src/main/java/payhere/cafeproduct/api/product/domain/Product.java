4615f416fd33bc54c961274e3e305ae799978ec3
pinomaker-hoo <inhoo23@naver.com>
2023-12-11 02:19:09 +0900

    fix : Product Model, remove password column, rename code column

--- src/main/java/payhere/cafeproduct/api/product/domain/Product.java
+++ src/main/java/payhere/cafeproduct/api/product/domain/Product.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
 import payhere.cafeproduct.api.productCategory.domain.ProductCategory;
-import payhere.cafeproduct.api.user.domain.User;
 import payhere.cafeproduct.global.domain.BaseTimeEntity;
 import payhere.cafeproduct.global.enums.ProductSize;
 
@@ -37,20 +36,16 @@ public class Product extends BaseTimeEntity {
     private String name;
 
     @Comment("상품 코드(바코드)")
-    @Column(nullable = false, length = 30, name = "cost")
+    @Column(nullable = false, length = 30, name = "code")
     private String code;
 
     @Comment("유통 기한")
-    @Column(nullable = false, name = "end_date")
-    private LocalDateTime endDate;
+    @Column(nullable = false, name = "expiration_date")
+    private LocalDateTime expirationDate;
 
     @Comment("사이즈")
     @Column(nullable = false, name = "size")
     private ProductSize size;
-
-    @Comment("비밀번호")
-    @Column(length = 150, nullable = false)
-    private String password;
 
     @Comment("매진 여부")
     @Column(columnDefinition = "CHAR(1)", nullable = false, name = "sold_out_yn")
