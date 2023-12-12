52c7183324978d9edf09cae83c00e2a1732bfca4
pinomaker-hoo <inhoo23@naver.com>
2023-12-12 23:00:50 +0900

    feat : Add Unit Test - Login User api

--- src/test/java/payhere/cafeproduct/user/service/UserServiceTest.java
+++ src/test/java/payhere/cafeproduct/user/service/UserServiceTest.java
@@ -11,10 +11,14 @@ import org.mockito.quality.Strictness;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import payhere.cafeproduct.api.user.event.dto.RequestUserLoginDto;
 import payhere.cafeproduct.api.user.event.dto.RequestUserSaveDto;
+import payhere.cafeproduct.api.user.event.vo.LoginUser;
 import payhere.cafeproduct.api.user.repository.UserJpaRepository;
 import payhere.cafeproduct.api.user.service.UserServiceImpl;
 import payhere.cafeproduct.global.exception.BadRequestException;
+import payhere.cafeproduct.global.exception.NotFoundException;
+import payhere.cafeproduct.global.jwt.JwtTokenProvider;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -35,7 +39,8 @@ public class UserServiceTest {
     @Mock
     private PasswordEncoder passwordEncoder;
 
-    }
+    @Mock
+    private JwtTokenProvider jwtTokenProvider;
 
     @Test
 
@@ -80,21 +85,66 @@ public class UserServiceTest {
 
     @Test
     public void 로그인_유저를_찾을_수_없습니다() throws Exception {
+        // Given
+        RequestUserLoginDto request = RequestUserLoginDto.builder()
+                .phoneNumber("010-1234-5678")
+                .password("qwer1595")
+                .build();
 
+        // Mock
+        when(userJpaRepository.existByPhoneNumber(anyString())).thenReturn(false);
+
+        // When
+        NotFoundException exception = assertThrows(NotFoundException.class, () -> {
+            userService.loginUser(request);
+        });
+
+        // Then
+        assertEquals("유저를 찾을 수 없습니다.", exception.getMessage());
     }
 
     @Test
     public void 로그인_비밀번호가_같지_않습니다() throws Exception {
+        // Given
+        RequestUserLoginDto request = RequestUserLoginDto.builder()
+                .phoneNumber("010-1234-5678")
+                .password("qwer1595")
+                .build();
 
+        LoginUser loginUser = new LoginUser(0, request.getPhoneNumber(), "");
+
+        // Mock
+        when(userJpaRepository.findUserByPhoneNumber(anyString())).thenReturn(loginUser);
+        when(passwordEncoder.matches(anyString(), anyString())).thenReturn(false);
+
+        // When
+        BadRequestException exception = assertThrows(BadRequestException.class, () -> {
+            userService.loginUser(request);
+        });
+
+        // Then
+        assertEquals("비밀번호가 같지 않습니다.", exception.getMessage());
     }
 
     @Test
-    public void 토큰재발급_토큰이_유효하지_않습니다() throws Exception {
+    public void 로그인_성공했습니다() throws Exception {
+        // Given
+        RequestUserLoginDto request = RequestUserLoginDto.builder()
+                .phoneNumber("010-1234-5678")
+                .password("qwer1595")
+                .build();
 
-    }
+        LoginUser loginUser = new LoginUser(0, request.getPhoneNumber(), request.getPassword());
 
-    @Test
-    public void 토큰재발급_토큰의_만료시간이_지났습니다() throws Exception {
+        // Mock
+        when(userJpaRepository.findUserByPhoneNumber(anyString())).thenReturn(loginUser);
+        when(passwordEncoder.matches(anyString(), anyString())).thenReturn(true);
 
+        // When
+        ResponseEntity<?> response = userService.loginUser(request);
+
+        // Then
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
     }
 }
