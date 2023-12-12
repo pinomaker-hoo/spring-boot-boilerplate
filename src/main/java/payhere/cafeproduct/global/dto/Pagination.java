8b87cd9a0c6a63418276e9d17b2321eafde31b24
pinomaker-hoo <inhoo23@naver.com>
2023-12-13 01:08:14 +0900

    feat : Add Pagination Response Dto

---
+++ src/main/java/payhere/cafeproduct/global/dto/Pagination.java
@@ -0,0 +1,23 @@
+package payhere.cafeproduct.global.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Pagination<T> {
+
+    private int totalPages;
+    private int currentPage;
+    private long totalItems;
+    private T data;
+
+    @Builder
+    public Pagination(int totalPages, int currentPage, long totalItems, T data) {
+        this.totalPages = totalPages;
+        this.currentPage = currentPage;
+        this.totalItems = totalItems;
+        this.data = data;
+    }
+}
