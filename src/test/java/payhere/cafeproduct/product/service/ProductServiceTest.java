fbeb38c3ea8275637ea45074db64d29e33c09d6e
pinomaker-hoo <inhoo23@naver.com>
2023-12-13 03:17:16 +0900

    feat : Add Test Code - Product Save

--- src/test/java/payhere/cafeproduct/product/service/ProductServiceTest.java
+++ src/test/java/payhere/cafeproduct/product/service/ProductServiceTest.java
@@ -15,6 +15,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import payhere.cafeproduct.api.log.repository.LogJpaRepository;
+import payhere.cafeproduct.api.product.domain.Product;
+import payhere.cafeproduct.api.product.event.dto.RequestProductSaveDto;
 import payhere.cafeproduct.api.product.repository.ProductJpaRepository;
 import payhere.cafeproduct.api.product.service.ProductServiceImpl;
 import payhere.cafeproduct.api.productCategory.domain.ProductCategory;
@@ -28,6 +30,7 @@ import payhere.cafeproduct.api.productCategory.service.ProductCategoryServiceImp
 import payhere.cafeproduct.api.user.domain.User;
 import payhere.cafeproduct.api.user.repository.UserJpaRepository;
 import payhere.cafeproduct.global.dto.UserDetailDto;
+import payhere.cafeproduct.global.enums.ProductSize;
 import payhere.cafeproduct.global.enums.UserRole;
 import payhere.cafeproduct.global.exception.ForbiddenException;
 import payhere.cafeproduct.global.exception.NotFoundException;
@@ -67,11 +70,61 @@ public class ProductServiceTest {
     @Test
     @DisplayName("상품 등록 - 상품 카테고리를 찾을 수 없습니다.")
     public void 상품_등록_카테고리를_찾을_수_없습니다() throws Exception {
+        // Given
+        RequestProductSaveDto dto = RequestProductSaveDto.builder()
+                .name("tea")
+                .price(30000)
+                .code("ABCD_EF@_123")
+                .cost(10000)
+                .productCategoryId(1)
+                .productSize(ProductSize.LARGE)
+                .exposeYn("Y")
+                .expirationDate("2024-12-31 12:00:00")
+                .build();
+
+        UserDetailDto userDetailDto = generateUserDetailDto();
+
+        // Mock
+        when(productCategoryJpaRepository.findById(anyInt())).thenReturn(Optional.empty());
+
+        // When
+        NotFoundException exception = assertThrows(NotFoundException.class, () -> {
+            productService.saveProduct(userDetailDto, dto);
+        });
+
+        // Then
+        assertEquals("상품 카테고리 정보를 찾을 수 없습니다.", exception.getMessage());
     }
 
     @Test
     @DisplayName("상품 등록 - 상품 등록에 성공했습니다.")
     public void 상품_등록_성공_했습니다() throws Exception {
+        // Given
+        RequestProductSaveDto dto = RequestProductSaveDto.builder()
+                .name("tea")
+                .price(30000)
+                .code("ABCD_EF@_123")
+                .cost(10000)
+                .productCategoryId(1)
+                .productSize(ProductSize.LARGE)
+                .exposeYn("Y")
+                .expirationDate("2024-12-31 12:00:00")
+                .build();
+
+        UserDetailDto userDetailDto = generateUserDetailDto();
+        ProductCategory productCategory = generateProductCategory();
+        Product product = generateProduct(productCategory);
+
+        // Mock
+        when(productCategoryJpaRepository.findById(anyInt())).thenReturn(Optional.ofNullable(productCategory));
+        when(productJpaRepository.save(any())).thenReturn(product);
+
+        // When
+        ResponseEntity<?> response = productService.saveProduct(userDetailDto, dto);
+
+        // Then
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
     }
 
     @Test
@@ -112,5 +165,24 @@ public class ProductServiceTest {
     @Test
     @DisplayName("상품 삭제 - 상품 삭제에 성공했습니다")
     public void 상품_삭제_성공_했습니다() throws Exception {
+    }
+
+    // ** Test UserDetailDto 생성
+    private UserDetailDto generateUserDetailDto() {
+        return UserDetailDto.builder().userId(1).role(UserRole.ROLE_MEMBER).build();
+    }
+
+
+    // ** Test Product Category 생성
+    private ProductCategory generateProductCategory() {
+        return ProductCategory.builder().id(1).name("tea").exposeYn("Y").build();
+    }
+
+    // ** Test Product 생성
+    private Product generateProduct(ProductCategory productCategory) {
+        return Product.builder().id(1L).productCategory(productCategory)
+                .name("아메리카노").price(30000).cost(10000).code("ABCD_EF@_123")
+                .exposeYn("N").size(ProductSize.LARGE).expirationDate(LocalDateTime.now())
+                .build();
     }
 }
